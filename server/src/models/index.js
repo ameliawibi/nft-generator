5fb31097f0d3fb5945f165a1c9e628a5989707c2
Amelia Wibisono <ameliawibi@gmail.com>
2022-08-08 13:34:03 +0800

    prep heroku prod config

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -2,6 +2,7 @@ import fs from "fs";
 import path from "path";
 import Sequelize from "sequelize";
 import enVariables from "../config/config.json";
+import url from "url";
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
@@ -9,8 +10,21 @@ const config = enVariables[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (env === "production") {
+  // Break apart the Heroku database url and rebuild the configs we need
+  const { DATABASE_URL } = process.env;
+  const dbUrl = url.parse(DATABASE_URL);
+  const username = dbUrl.auth.substr(0, dbUrl.auth.indexOf(":"));
+  const password = dbUrl.auth.substr(
+    dbUrl.auth.indexOf(":") + 1,
+    dbUrl.auth.length
+  );
+  const dbName = dbUrl.path.slice(1);
+  const host = dbUrl.hostname;
+  const { port } = dbUrl;
+  config.host = host;
+  config.port = port;
+  sequelize = new Sequelize(dbName, username, password, config);
 } else {
   sequelize = new Sequelize(
     config.database,
