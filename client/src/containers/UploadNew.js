83f5d92c3990d07172db4f3dded4b16e060ab090
Amelia Wibisono <ameliawibi@gmail.com>
2022-08-26 01:30:36 +0800

    class to className

--- client/src/containers/UploadNew.js
+++ client/src/containers/UploadNew.js
@@ -28,19 +28,19 @@ export default function UploadNew() {
   return (
     <div>
       <label
-        class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
         htmlFor="file_input"
       >
         Upload file
       </label>
       <p
-        class="mt-1 text-sm text-gray-500 dark:text-gray-300"
+        className="mt-1 text-sm text-gray-500 dark:text-gray-300"
         id="file_input_help"
       >
         ZIP File (Max 90 MB)
       </p>
       <input
-        class="block w-1/5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 mb-2"
+        className="block w-1/5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 mb-2"
         aria-describedby="file_input_help"
         id="file_input"
         type="file"
